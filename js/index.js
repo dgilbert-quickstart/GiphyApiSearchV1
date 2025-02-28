d378b34cc408dd671d8ec105f1033c61329dc271
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-28 22:52:09 +0000

    add scss , update index.js, index.html

--- js/index.js
+++ js/index.js
@@ -231,7 +231,7 @@ function btnSearch(){
             }
 
             _image = data.data[i].images.original.url;
-            _html += `<img width='100' height='100' src='${_image}' style='padding:5px'>`
+            _html += `<img width='200' height='200' src='${_image}' style='padding:5px'>`
             //display 3 images per row 
           }
           
